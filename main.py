fcc050f49173a3c9fd9a9cf355eb628f5f6152dd
pauldeguire <66140813+pauldeguire@users.noreply.github.com>
2023-01-01 20:19:29 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import interface.interface_pipopipette
+from tkinter import *
+
+if __name__ == '__main__':
+    # Main de votre programme. Crée la fenêtre et la fait afficher.
+    f = interface.interface_pipopipette.Fenetre()
+    f.mainloop()
